364d4bf262c907f4251e36235b3397a33105e3d5
Tiago Varela <vogait@gmail.com>
2012-12-04 15:42:08 +0000

    Fixed visualizer. Damn you atan2

--- collaborativeRobot/src/BeaconVisualizer.java
+++ collaborativeRobot/src/BeaconVisualizer.java
@@ -3,47 +3,68 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import sun.text.normalizer.UBiDiProps;
 
 public class BeaconVisualizer extends Thread {
 
   @Override
   public void run() {
     JPanel panel = new JPanel();
-		JFrame frame = new JFrame("Beacon Visualizer");
-		frame.add(panel);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(jClient.mapSizeX, jClient.mapSizeY);
-		panel.setBackground(Color.WHITE);
+    JFrame frame = new JFrame("Beacon Visualizer");
+    frame.add(panel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(Constants.mapSizeX, Constants.mapSizeY);
+    panel.setBackground(Color.WHITE);
     frame.setVisible(true);
 
+    int index = 0;
     while (true) {
       Graphics g = panel.getGraphics();
-      for (int i = 0; i < jClient.beaconProbability.length; i++) {
-        for (int j = 0; j < jClient.beaconProbability[i].length; j++) {
-          if(jClient.beacon.beaconVisible){
-            double beaconDir = jClient.beacon.beaconDir;
-            double angleI = beaconDir - 2;
-            double angleF = beaconDir + 2;
-            int centerPosX = (int) jClient.PosX;
-            int centerPosY = (int) jClient.PosY;
-            calculateVisibleArea(centerPosX, centerPosY, angleI, angleF, g, i, j);
+      if (jClient.beacon.beaconVisible) {
+        
+        double robotDir = jClient.compass;
+        double beaconRelDir = jClient.beacon.beaconDir;
+        double beaconDir = robotDir + beaconRelDir;
+        System.out.println("beacon dir:" + beaconDir);
+        double angleI = Math.toRadians(beaconDir-1);
+        double angleF = angleI + Math.toRadians(1);
+        
+        angleI = normalizeAngle(angleI);
+        angleF = normalizeAngle(angleF);
+        
+        int centerPosX = (int) jClient.PosX;
+        int centerPosY = (int) jClient.PosY;
+
+        for (int y = 0; y < jClient.beaconProbability.length; y++) {
+          for (int x = 0; x < jClient.beaconProbability[y].length; x++) {
+            calculateVisibleArea(centerPosX, centerPosY, angleI, angleF, g, x, y);
           }
         }
-      }    
+      }
       g.dispose();
     }
   }
   
-  private static void calculateVisibleArea(int centerPosX2,int centerPosY2,
-          double angleCenterI, double angleCenterF, Graphics g, int i, int j) {
-		
-		double anglePoint;
-		anglePoint = Math.atan2(j - centerPosX2, centerPosY2 - i);
+  double normalizeAngle(double angle) {
+    double newAngle = angle;
+    while (newAngle <= -Math.PI) {
+      newAngle += 2*Math.PI;
+    }
+    while (newAngle > Math.PI) {
+      newAngle -= 2*Math.PI;
+    }
+    return newAngle;
+  }
 
-		if (anglePoint > angleCenterI && anglePoint < angleCenterF) {
-			g.setColor(Color.RED);
-			g.fillRect(j, i, 1, 1);
-		}
-	}
-    
+  private static void calculateVisibleArea(int cx, int cy,
+          double ai, double af, Graphics g, int x, int y) {
+
+    double anglePoint;
+    anglePoint = Math.atan2(cy - y, x - cx);
+
+    if (anglePoint >= ai && anglePoint <= af) {
+      g.setColor(Color.RED);
+      g.fillRect(x, y, 1, 1);
+    }
+  }
 }
