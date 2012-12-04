b8358663c9fb15fb6eba0cfc0593456516b96df9
Tiago Varela <vogait@gmail.com>
2012-12-04 18:42:35 +0000

    Added better beacon visualizer

--- collaborativeRobot/src/BeaconVisualizer.java
+++ collaborativeRobot/src/BeaconVisualizer.java
@@ -21,17 +21,17 @@ public class BeaconVisualizer extends Thread {
     while (true) {
       Graphics g = panel.getGraphics();
       if (jClient.beacon.beaconVisible) {
-        
+
         double robotDir = jClient.compass;
         double beaconRelDir = jClient.beacon.beaconDir;
         double beaconDir = robotDir + beaconRelDir;
         System.out.println("beacon dir:" + beaconDir);
-        double angleI = Math.toRadians(beaconDir-1);
+        double angleI = Math.toRadians(beaconDir - 1);
         double angleF = angleI + Math.toRadians(1);
-        
+
         angleI = normalizeAngle(angleI);
         angleF = normalizeAngle(angleF);
-        
+
         int centerPosX = (int) jClient.PosX;
         int centerPosY = (int) jClient.PosY;
 
@@ -44,14 +44,14 @@ public class BeaconVisualizer extends Thread {
       g.dispose();
     }
   }
-  
+
   double normalizeAngle(double angle) {
     double newAngle = angle;
     while (newAngle <= -Math.PI) {
-      newAngle += 2*Math.PI;
+      newAngle += 2 * Math.PI;
     }
     while (newAngle > Math.PI) {
-      newAngle -= 2*Math.PI;
+      newAngle -= 2 * Math.PI;
     }
     return newAngle;
   }
@@ -63,8 +63,19 @@ public class BeaconVisualizer extends Thread {
     anglePoint = Math.atan2(cy - y, x - cx);
 
     if (anglePoint >= ai && anglePoint <= af) {
-      g.setColor(Color.RED);
-      g.fillRect(x, y, 1, 1);
+      jClient.beaconProbability[y][x] += 0.1;
+      if (jClient.beaconProbability[y][x] > 0.3 &&
+              jClient.beaconProbability[y][x] <= 0.4) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, 1, 1);
+      } else if(jClient.beaconProbability[y][x] > 0.8 &&
+              jClient.beaconProbability[y][x] <= 1.2) {
+        g.setColor(Color.ORANGE);
+        g.fillRect(x, y, 1, 1);
+      } else if(jClient.beaconProbability[y][x] > 1.2) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 1, 1);
+      }
     }
   }
 }
