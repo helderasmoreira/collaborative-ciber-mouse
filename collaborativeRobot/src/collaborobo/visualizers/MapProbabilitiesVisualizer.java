c7415e43337d8a218ebe309659ea6250ae9386d4
pedrotiagopontes <pedrotiagopontes@gmail.com>
2012-12-11 02:59:18 +0000

    probabilities gradient

--- collaborativeRobot/src/collaborobo/visualizers/MapProbabilitiesVisualizer.java
+++ collaborativeRobot/src/collaborobo/visualizers/MapProbabilitiesVisualizer.java
@@ -31,16 +31,52 @@ public class MapProbabilitiesVisualizer extends Thread {
 
     while (true) {
       Graphics g = panel.getGraphics();
+      int wallColor = 255;
+      int colorStep = wallColor/6;      
 
       for (int y = 0; y < robot.probabilitiesMap.length; y++) {
         for (int x = 0; x < robot.probabilitiesMap[y].length; x++) {
-          if (robot.probabilitiesMap[y][x] > 0.50) {
-            g.setColor(Color.RED);
-            g.fillRect(x, y, 1, 1);
-          } else if (robot.probabilitiesMap[y][x] < 0.5) {
-            g.setColor(Color.BLUE);
-            g.fillRect(x, y, 1, 1);
-          }
+            double cellProb = robot.probabilitiesMap[y][x];
+            if (cellProb >= 1) {
+                g.setColor(new Color(wallColor, 0, 0));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.9){
+                g.setColor(new Color(wallColor, colorStep, colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.8){
+                g.setColor(new Color(wallColor, 2*colorStep, 2*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.7){
+                g.setColor(new Color(wallColor, 3*colorStep, 3*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.6){
+                g.setColor(new Color(wallColor, 4*colorStep, 4*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb > 0.5){
+                g.setColor(new Color(wallColor, 5*colorStep, 5*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb == 0.5){
+                g.setColor(Color.WHITE);
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.4){
+                g.setColor(new Color(255-colorStep, 255-colorStep, 255-colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.3){
+                g.setColor(new Color(255-2*colorStep, 255-2*colorStep, 255-2*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.2){
+                g.setColor(new Color(255-3*colorStep, 255-3*colorStep, 255-3*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb >= 0.1){
+                g.setColor(new Color(255-4*colorStep, 255-4*colorStep, 255-4*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb > 0.0){
+                g.setColor(new Color(255-5*colorStep, 255-5*colorStep, 255-5*colorStep));
+                g.fillRect(x, y, 1, 1);
+            }else if(cellProb == 0.0){
+                g.setColor(Color.BLACK);
+                g.fillRect(x, y, 1, 1);
+            }
         }
       }
 
